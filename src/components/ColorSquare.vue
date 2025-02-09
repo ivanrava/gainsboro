55a96e8103a18174c2b88947af66709c67bde6f4
Ivan Ravasi <ivan4.ravasi@gmail.com>
2025-02-09 01:14:35 +0100

    Adds basic color list

---
+++ src/components/ColorSquare.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  color: Color
+}>()
+</script>
+
+<template>
+  <article class="w-24 h-24 p-2" :style="{backgroundColor: color.hex}">
+    {{ color.name }}
+  </article>
+</template>
---
+++ src/types/types.d.ts
@@ -0,0 +1,4 @@
+interface Color {
+  name: string;
+  hex: string;
+}
